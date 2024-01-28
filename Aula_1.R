271e36c7e6b79e00d4cc14b16cb12ac15a7373e5
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2024-01-28 15:08:08 -0300

    Add files via upload

---
+++ Aula_1.R
@@ -0,0 +1,34 @@
+# OBJETIVO: primeiro contato com a linguagem R e RStudio
+
+# COMO?
+# navegação e exemplos
+
+# Criando Variáveis
+# Para executar clique sobre a linha ou selecione o conjunto de linhas
+# e pressione CTRL + Enter
+
+a <- 10
+a
+
+b = 5
+b
+B # Ele faz distinção entre maiúscula e minúscula
+
+
+# Utilizaremos <- na criação de  variáveis e = nas atribuições de funções
+
+c -> 10 # não é possível atribuir letra a um número
+10 -> c # é possível atribuir número a uma letra
+c -> d
+
+# Para padronizar utilizaremos "variável" <- "valor"
+
+a <- 8
+b <- 7
+
+c <- a + b
+
+a <- "João"
+b <- "Maria"
+
+c <- a + b # não é possível somar "character"
