a6a85c1400a12893175f6816f188a1f9478b2c29
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2024-01-29 19:41:20 -0300

    Add files via upload

---
+++ Aula_3.R
@@ -0,0 +1,62 @@
+# OBJETIVO: realizar operações matemáticas, lógicas e de comparação
+
+# COMO?
+# utilizanod recursos básicos
+
+# Soma
+5 + 5
+
+#Subtração
+10 - 8
+
+# Divisão
+9 / 3
+
+# Multiplicação
+4 * 2
+
+# Potência
+4 ^ 2
+4**2
+
+# Igual?
+5 == 6
+5 == 5
+
+# Diferente?
+5 !=6
+5 != 5
+
+# Maior?
+4 > 2
+4 > 6
+
+# Menor?
+3 < 2
+3 < 4
+
+# Maior ou igual?
+4 >= 5
+4 >= 4
+
+# Menor ou igual?
+6 <= 8
+6 <= 5
+
+# E
+6 == 6
+6 == 6 & 7 == 8
+6 == 6 & 7 != 8
+5 > 3 & 5 < 8
+
+# OU
+6 == 6
+7 == 8
+6 == 6 | 7 == 8
+
+6 != 6 | 7 == 8
+5 > 3 | 5 < 8
+
+# Negação
+!6 == 6
+!4 > 2
