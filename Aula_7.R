e28c5958c964526008d0aeff179d019a7f0f100d
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2024-01-29 20:21:34 -0300

    Add files via upload

---
+++ Aula_7.R
@@ -0,0 +1,19 @@
+# OBJETIVO: entender como a linguagem R organiza os dados
+
+# COMO?
+# exemplos
+
+# Armazenamento de lógico
+Salario <- 3450.89
+Horas <- 220
+
+L1 <- Salario > Horas
+L1
+
+L2 <- Salario < Horas
+L2
+
+Logico <- TRUE
+Logico1 <- "TRUE"
+Logico2 <- c(1, TRUE, 3)
+Logico2
