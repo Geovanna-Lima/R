584e7bc1355e1c983c0ac3ff79b75abe1ea49fc4
Geovanna da S. Lima <71272198+Geovanna-Lima@users.noreply.github.com>
2024-01-29 19:59:16 -0300

    Add files via upload

---
+++ Aula_5.R
@@ -0,0 +1,20 @@
+# OBJETIVO: entender como a linguagem R organiza os dados
+
+# COMO?
+# exemplos
+
+# Armazenamento de caracteres
+Nome1 <- "Eduardo Abreu"
+Nome2 <- "Amanda Lopes"
+Idade <- 25
+
+Idade + 5
+
+Nomes <- Nome1 + Nome2 # Não é possível realizar
+Nomes <- c(Nome1, Nome2)
+
+Nomes
+Nomes[1]
+Nomes[2]
+
+Nome1 == Nome2
